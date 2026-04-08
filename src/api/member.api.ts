16b506d255e1e755942e8152bd482c4ab013f8de
rladbwjd0817 <rladbwjd0817@gmail.com>
2026-04-08 16:20:15 +0900

    Fix: 회원가입 시 인증번호 유효성검사중

--- src/api/member.api.ts
+++ src/api/member.api.ts
@@ -62,17 +62,30 @@ export const postLogin = async (loginData: object) => {
 };
 
 /**
- * 
+ *
  * @param authCode 인증번호
- * @returns 
+ * @returns
  */
 export const postAuthCode = async (authCode: object) => {
-  try{
+  try {
     const response = await api.post("/authes/authCode", authCode);
     return response;
-  }catch(e){
+  } catch (e) {
     console.log("인증번호 생성 중 오류 발생", e);
     throw e;
   }
 };
 
+/**
+ * 회원가입 시 인증번호 유효성검사
+ * @returns 
+ */
+export const postCheckFarmerAuth = async (authCode: object) => {
+  try {
+    const response = await api.post("/authes/check-auth", authCode);
+    return response;
+  } catch (e) {
+    console.log("인증번호 유효성 검사 시 오류 발생", e);
+    throw e;
+  }
+};
