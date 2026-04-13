9a9ab91da53804a9af4ca992252e94448d1936dc
rladbwjd0817 <rladbwjd0817@gmail.com>
2026-04-13 17:22:42 +0900

    fix: 미등록 농장주 count 출력

--- src/queries/admin.queries.ts
+++ src/queries/admin.queries.ts
@@ -1,4 +1,4 @@
-import { selectFarmerList } from "@/api/admin.api";
+import { selectFarmerList, selectUnregFarmerCount } from "@/api/admin.api";
 import { useQuery } from "@tanstack/react-query";
 
 // 인증번호를 가진 농장주 조회
@@ -6,5 +6,13 @@ export const useGetFarmerList = () => {
   return useQuery({
     queryKey: ["farmers"],
     queryFn: selectFarmerList,
+  });
+};
+
+// 미등록 농장주count 조회
+export const useSelectUnregFarmerCount = () => {
+  return useQuery({
+    queryKey: ["selectUnregFarmerCount"],
+    queryFn: () => selectUnregFarmerCount(),
   });
 };
